7e755bbc1f461e5adc134498def7f94f76ba6aed
Choimin0301 <98930093+Choimin0301@users.noreply.github.com>
2023-04-06 11:41:52 +0900

    dificultmodechangevibe
    easy/hard모드 조정시 바이브 기능 추가 및 바이브레이션 매니저 생성

---
+++ Assets/01.Scripts/Core/VibeManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibeManager : MonoBehaviour
+{
+    
+    public void VibeLight()
+    {
+        Taptic.Light();
+    }
+}
